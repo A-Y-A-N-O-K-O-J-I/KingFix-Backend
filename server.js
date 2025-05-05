d12c150f7f846332d8e874b2cd722ee355fd9345
ayanokojix23 <ayanokojix2306@gmail.com>
2025-05-05 06:29:06 +0000

    lol

---
+++ server.js
@@ -0,0 +1,37 @@
+// /api/contact.js
+require('dotenv').config();
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: 'smtp-relay.brevo.com',
+  port: 587,
+  auth: {
+    user: process.env.SMTP_USER,
+    pass: process.env.SMTP_PASS
+  }
+});
+
+module.exports = async (req, res) => {
+  if (req.method === 'POST') {
+    const { name, email, message } = req.body;
+
+    const mailOptions = {
+      from: '"KingFix Support" <support@kingfix.name.ng>',
+      to: 'support@kingfix.name.ng',
+      subject: `New Message from ${name}`,
+      html: `<p>You have received a new message from your website contact form.</p>
+             <p><strong>Name:</strong> ${name}</p>
+             <p><strong>Email:</strong> ${email}</p>
+             <p><strong>Message:</strong> ${message}</p>`
+    };
+
+    try {
+      await transporter.sendMail(mailOptions);
+      res.status(200).json({ message: 'Email sent successfully' });
+    } catch (error) {
+      res.status(500).json({ error: 'Error sending email' });
+    }
+  } else {
+    res.status(405).json({ error: 'Method Not Allowed' });
+  }
+};
